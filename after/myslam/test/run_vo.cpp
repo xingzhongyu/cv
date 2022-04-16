681d4bdae71acff7d15c6ce930e80a20a1afebfe
xzy <xzy12386@163.com>
2022-04-16 15:47:59 +0800

    test

--- after/myslam/test/run_vo.cpp
+++ after/myslam/test/run_vo.cpp
@@ -59,17 +59,17 @@ int main ( int argc, char** argv )
     myslam::Camera::Ptr camera ( new myslam::Camera );
 
     // visualization
-    cv::viz::Viz3d vis ( "Visual Odometry" );
-    cv::viz::WCoordinateSystem world_coor ( 1.0 ), camera_coor ( 0.5 );
-    cv::Point3d cam_pos ( 0, -1.0, -1.0 ), cam_focal_point ( 0,0,0 ), cam_y_dir ( 0,1,0 );
+    // cv::viz::Viz3d vis ( "Visual Odometry" );
+    // cv::viz::WCoordinateSystem world_coor ( 1.0 ), camera_coor ( 0.5 );
+    // cv::Point3d cam_pos ( 0, -1.0, -1.0 ), cam_focal_point ( 0,0,0 ), cam_y_dir ( 0,1,0 );
 
-    cv::Affine3d cam_pose = cv::viz::makeCameraPose ( cam_pos, cam_focal_point, cam_y_dir );
-    vis.setViewerPose ( cam_pose );
+    // cv::Affine3d cam_pose = cv::viz::makeCameraPose ( cam_pos, cam_focal_point, cam_y_dir );
+    // vis.setViewerPose ( cam_pose );
 
-    world_coor.setRenderingProperty ( cv::viz::LINE_WIDTH, 2.0 );
+    // world_coor.setRenderingProperty ( cv::viz::LINE_WIDTH, 2.0 );
     camera_coor.setRenderingProperty ( cv::viz::LINE_WIDTH, 1.0 );
-    vis.showWidget ( "World", world_coor );
-    vis.showWidget ( "Camera", camera_coor );
+    // vis.showWidget ( "World", world_coor );
+    // vis.showWidget ( "Camera", camera_coor );
 
     cout<<"read total "<<rgb_files.size() <<" entries"<<endl;
     for ( int i=0; i<rgb_files.size(); i++ )
@@ -91,27 +91,27 @@ int main ( int argc, char** argv )
 
         if ( vo->state_ == myslam::VisualOdometry::LOST )
             break;
-        SE3 Twc = pFrame->T_c_w_.inverse();
+        // SE3 Twc = pFrame->T_c_w_.inverse();
 
         // show the map and the camera pose
-        cv::Affine3d M (
-                cv::Affine3d::Mat3 (
-                        Twc.rotation_matrix() ( 0,0 ), Twc.rotation_matrix() ( 0,1 ), Twc.rotation_matrix() ( 0,2 ),
-                        Twc.rotation_matrix() ( 1,0 ), Twc.rotation_matrix() ( 1,1 ), Twc.rotation_matrix() ( 1,2 ),
-                        Twc.rotation_matrix() ( 2,0 ), Twc.rotation_matrix() ( 2,1 ), Twc.rotation_matrix() ( 2,2 )
-                ),
-                cv::Affine3d::Vec3 (
-                        Twc.translation() ( 0,0 ), Twc.translation() ( 1,0 ), Twc.translation() ( 2,0 )
-                )
-        );
+        // cv::Affine3d M (
+        //         cv::Affine3d::Mat3 (
+        //                 Twc.rotation_matrix() ( 0,0 ), Twc.rotation_matrix() ( 0,1 ), Twc.rotation_matrix() ( 0,2 ),
+        //                 Twc.rotation_matrix() ( 1,0 ), Twc.rotation_matrix() ( 1,1 ), Twc.rotation_matrix() ( 1,2 ),
+        //                 Twc.rotation_matrix() ( 2,0 ), Twc.rotation_matrix() ( 2,1 ), Twc.rotation_matrix() ( 2,2 )
+        //         ),
+        //         cv::Affine3d::Vec3 (
+        //                 Twc.translation() ( 0,0 ), Twc.translation() ( 1,0 ), Twc.translation() ( 2,0 )
+        //         )
+        // );
 
-        Mat img_show = color.clone();
-        for ( auto& pt:vo->map_->map_points_ )
-        {
-            myslam::MapPoint::Ptr p = pt.second;
-            Vector2d pixel = pFrame->camera_->world2pixel ( p->pos_, pFrame->T_c_w_ );
-            cv::circle ( img_show, cv::Point2f ( pixel ( 0,0 ),pixel ( 1,0 ) ), 5, cv::Scalar ( 0,255,0 ), 2 );
-        }
+        // Mat img_show = color.clone();
+        // for ( auto& pt:vo->map_->map_points_ )
+        // {
+        //     myslam::MapPoint::Ptr p = pt.second;
+        //     Vector2d pixel = pFrame->camera_->world2pixel ( p->pos_, pFrame->T_c_w_ );
+        //     cv::circle ( img_show, cv::Point2f ( pixel ( 0,0 ),pixel ( 1,0 ) ), 5, cv::Scalar ( 0,255,0 ), 2 );
+        // }
 
         cv::imshow ( "image", img_show );
         cv::waitKey ( 1 );
