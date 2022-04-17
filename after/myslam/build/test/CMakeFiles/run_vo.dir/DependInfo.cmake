996cf873fc6a67c3a6c3bfc698bd21d69832c2d7
xzy <xzy12386@163.com>
2022-04-16 20:46:50 -0700

    test

--- after/myslam/build/test/CMakeFiles/run_vo.dir/DependInfo.cmake
+++ after/myslam/build/test/CMakeFiles/run_vo.dir/DependInfo.cmake
@@ -1,20 +1,44 @@
-
-# Consider dependencies only in project.
-set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
-
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/xzy/cv/after/myslam/test/run_vo.cpp" "/home/xzy/cv/after/myslam/build/test/CMakeFiles/run_vo.dir/run_vo.cpp.o"
+  "/home/xzy/cv/after/myslam/test/run_vo2.cpp" "/home/xzy/cv/after/myslam/build/test/CMakeFiles/run_vo.dir/run_vo2.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DISABLE_LIBUSB_1_0"
+  "DISABLE_PCAP"
+  "DISABLE_PNG"
+  "GFLAGS_DLL_DECLARE_FLAG="
+  "GFLAGS_DLL_DEFINE_FLAG="
+  "GFLAGS_IS_A_DLL=0"
+  "GOOGLE_GLOG_DLL_DECL="
+  "qh_QHpointer"
+  "vtkRenderingContext2D_AUTOINIT=1(vtkRenderingContextOpenGL2)"
+  "vtkRenderingCore_AUTOINIT=3(vtkInteractionStyle,vtkRenderingFreeType,vtkRenderingOpenGL2)"
   )
 
-# The set of dependency files which are needed:
-set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/home/guojiawei/cv/cv2/cv/after/myslam/test/run_vo.cpp" "test/CMakeFiles/run_vo.dir/run_vo.cpp.o" "gcc" "test/CMakeFiles/run_vo.dir/run_vo.cpp.o.d"
-  "/home/guojiawei/cv/cv2/cv/after/myslam/test/run_vo2.cpp" "test/CMakeFiles/run_vo.dir/run_vo2.cpp.o" "gcc" "test/CMakeFiles/run_vo.dir/run_vo2.cpp.o.d"
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/xzy/Downloads/Sophus"
+  "../include"
+  "/usr/include/eigen3"
+  "/usr/include/opencv4"
+  "/usr/include/vtk-7.1"
+  "/usr/include/freetype2"
+  "/usr/include/pcl-1.10"
+  "/usr/include/ni"
+  "/usr/include/openni2"
   )
 
 # Targets to which this target links.
 set(CMAKE_TARGET_LINKED_INFO_FILES
-  "/home/guojiawei/cv/cv2/cv/after/myslam/build/src/CMakeFiles/myslam.dir/DependInfo.cmake"
+  "/home/xzy/cv/after/myslam/build/src/CMakeFiles/myslam.dir/DependInfo.cmake"
   )
 
 # Fortran module output directory.
--- after/myslam/build/test/CMakeFiles/run_vo.dir/cmake_clean.cmake
+++ after/myslam/build/test/CMakeFiles/run_vo.dir/cmake_clean.cmake
@@ -2,9 +2,7 @@ file(REMOVE_RECURSE
   "../../bin/run_vo"
   "../../bin/run_vo.pdb"
   "CMakeFiles/run_vo.dir/run_vo.cpp.o"
-  "CMakeFiles/run_vo.dir/run_vo.cpp.o.d"
   "CMakeFiles/run_vo.dir/run_vo2.cpp.o"
-  "CMakeFiles/run_vo.dir/run_vo2.cpp.o.d"
 )
 
 # Per-language clean rules from dependency scanning.
